eb8afbfa417ab7aae13bea9fb827edc2528647d0
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-11-02 18:19:53 -0700

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow"
 import { ExpandMoreOutlined } from '@mui/icons-material'
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 import EmojiFlagsIcon from'@mui/icons-material/EmojiFlags';
 import PeopleIcon from '@mui/icons-material/People';
 import ChatIcon from '@mui/icons-material/Chat';
@@ -16,12 +15,8 @@ function Sidebar() {
     return (
         <div className="sidebar">
         <SidebarRow src={user.photoURL} title={user.displayName} />
-        <SidebarRow
-          Icon={LocalHospitalIcon}
-          title="COVID-19 Information Center"
-        />
-        <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
         <SidebarRow Icon={PeopleIcon} title="Friends" />
+        <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
         <SidebarRow Icon={ChatIcon} title="Messenger" />
         <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
         <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
